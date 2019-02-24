7f8a21254f42c9aa004994c36fb76b882b4c6811
Roi Driscoll <roi.driscoll@gmail.com>
2019-02-24 16:20:02 +0000

    Adds fleamail json file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import allFleaMails from "./all_fleamails.json";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+function App() {
+  const firstEntry = allFleaMails.data[0].entries[0];
+  const secondEntry = allFleaMails.data[1].entries[0];
+
+  return (
+    <div className="App">
+      <DiaryEntry title="whatever" entry={firstEntry} />
+      <DiaryEntry title="also whatever" entry={secondEntry} />
+    </div>
+  );
+}
+
+
+function DiaryEntry(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <ul>
+        <li>Author: {props.entry.author}</li>
+        <li>Date: {props.entry.date}</li>
+        <li>Recipient: {props.entry.recipient}</li>
+        <li>Subject: {props.entry.subject}</li>
+        <li>Body: {props.entry.body}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
